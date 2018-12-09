9ce0975a5d5e20d9b3c4ab34c826a11a5eb5f35a
Madhu <madhu@casaone.com>
2018-12-09 19:43:44 +0530

    change icons

--- resources/assets/react/components/SideBar/SideBar.jsx
+++ resources/assets/react/components/SideBar/SideBar.jsx
@@ -15,32 +15,56 @@ function Sidebar(props) {
                     <div className="col-md-12 sidebar-tool-box-nav-bar">
                         <div className={`sidebar-tool-box-nav-button ${props.pageName === 'mediaUpload' ? 'active' : '' }`}>
                             <Link to={`/upload`}>
-                                <img className="sidebarIcon" src="https://s3.us-east-2.amazonaws.com/soundwave-assets/images/icons8-sound-50.png" />
+                                {
+                                    props.pageName === 'mediaUpload' ?
+                                    <img className="sidebarIcon" src="https://s3.us-east-2.amazonaws.com/soundwave-assets/images/icons8-white-audio-50.png" /> :
+                                    <img className="sidebarIcon" src="https://s3.us-east-2.amazonaws.com/soundwave-assets/images/icons8-audio-50-1.png" />
+                                }
                             </Link>
                         </div>
                         <div className={`sidebar-tool-box-nav-button ${props.pageName === 'color' ? 'active' : '' }`}>
                             <Link to={`/${match.params.mediaId}/color`}>
-                                <img className="sidebarIcon" src="https://s3.us-east-2.amazonaws.com/soundwave-assets/images/icons8-paint-palette-50.png" />
+                                {
+                                    props.pageName === 'color' ?
+                                    <img className="sidebarIcon" src="https://s3.us-east-2.amazonaws.com/soundwave-assets/images/icons8-white-paint-palette-50.png" /> :
+                                    <img className="sidebarIcon" src="https://s3.us-east-2.amazonaws.com/soundwave-assets/images/icons8-paint-palette-50-1.png" />
+                                }
                             </Link>
                         </div>
                         <div className={`sidebar-tool-box-nav-button ${props.pageName === 'style' ? 'active' : '' }`}>
                             <Link to={`/${match.params.mediaId}/style`}>
-                                <img className="sidebarIcon" src="https://s3.us-east-2.amazonaws.com/soundwave-assets/images/icons8-tune-50.png" />
+                                {
+                                    props.pageName === 'style' ?
+                                    <img className="sidebarIcon" src="https://s3.us-east-2.amazonaws.com/soundwave-assets/images/icons8-white-tune-50.png" /> :
+                                    <img className="sidebarIcon" src="https://s3.us-east-2.amazonaws.com/soundwave-assets/images/icons8-tune-50-1.png" />
+                                }
                             </Link>
                         </div>
                         <div className={`sidebar-tool-box-nav-button ${props.pageName === 'text' ? 'active' : '' }`}>
                             <Link to={`/${match.params.mediaId}/text`}>
-                                <img className="sidebarIcon" src="https://s3.us-east-2.amazonaws.com/soundwave-assets/images/icons8-text-color-50.png" />
+                                {
+                                    props.pageName === 'text' ?
+                                    <img className="sidebarIcon" src="https://s3.us-east-2.amazonaws.com/soundwave-assets/images/icons8-white-text-color-50.png" /> :
+                                    <img className="sidebarIcon" src="https://s3.us-east-2.amazonaws.com/soundwave-assets/images/icons8-text-color-50-1.png" />
+                                }
                             </Link>
                         </div>
                         <div className={`sidebar-tool-box-nav-button ${props.pageName === 'qrCode' ? 'active' : '' }`}>
                             <Link to={`/${match.params.mediaId}/qr-code`}>
-                                <img className="sidebarIcon" src="https://s3.us-east-2.amazonaws.com/soundwave-assets/images/icons8-qr-code-50.png" />
+                                {
+                                    props.pageName === 'qrCode' ?
+                                    <img className="sidebarIcon" src="https://s3.us-east-2.amazonaws.com/soundwave-assets/images/icons8-white-qr-code-50.png" /> :
+                                    <img className="sidebarIcon" src="https://s3.us-east-2.amazonaws.com/soundwave-assets/images/icons8-qr-code-50-1.png" />
+                                }
                             </Link>
                         </div>
                         <div className={`sidebar-tool-box-nav-button ${props.pageName === 'printOption' ? 'active' : '' }`}>
                             <Link to={`/${match.params.mediaId}/print-option`}>
-                                <img className="sidebarIcon" src="https://s3.us-east-2.amazonaws.com/soundwave-assets/images/icons8-print-50.png" />
+                                {
+                                    props.pageName === 'printOption' ?
+                                    <img className="sidebarIcon" src="https://s3.us-east-2.amazonaws.com/soundwave-assets/images/icons8-white-print-50.png" /> :
+                                    <img className="sidebarIcon" src="https://s3.us-east-2.amazonaws.com/soundwave-assets/images/icons8-print-50-1.png" />
+                                }
                             </Link>
                         </div>
                     </div>

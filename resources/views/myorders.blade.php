4947ab9b5d6ee297f0cab2d622cce6bfb72bc474
Madhu <madhu@casaone.com>
2018-12-30 15:37:14 +0530

    Fix

--- resources/views/myorders.blade.php
+++ resources/views/myorders.blade.php
@@ -1,5 +1,6 @@
 
 @extends('layouts.app')
+@section('metadata')
 <title>My Orders</title>
 <meta name="description" content="Create a sound wave picture from your sound. You can have a sound wave pic of your wedding vow, favorite song, or any sound you can think of!">
 <meta name="keywords" content="SoundWave, Sound Wave Picture, SoundWave Art, Soundviz, Sound wave pic">

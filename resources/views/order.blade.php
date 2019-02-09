ae92528524056b1fb2fe8d2fa2b963e0d6bd7d3f
Madhu <madhu@casaone.com>
2019-02-09 17:02:00 +0530

    Add address

--- resources/views/order.blade.php
+++ resources/views/order.blade.php
@@ -13,6 +13,20 @@
             @include('partials.order_detail')
         </div>
     </div>
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="order-container">
+                <h4>Shipping Address</h4>
+                <div>{{$order->address->first_name}} {{ $order->address->last_name }}</div>
+                <div>{{$order->address->address_line_1}} {{ $order->address->address_line_2 }}</div>
+                <div>{{$order->address->city}}</div>
+                <div>{{$order->address->state}} {{ $order->address->zipcode }}</div>
+                <div>{{$order->address->country->country_name}}</div>
+                <div>Phone: {{$order->address->phone_number}}</div>
+                <div>Email: {{$order->address->email}} </div>
+            </div>
+        </div>
+    </div>
 </div>
 @include('partials.footer')
 @endsection

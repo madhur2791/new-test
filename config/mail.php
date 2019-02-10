38504b19665dd7073aa5a8422c1460dbe94b0d47
Madhu <madhu@casaone.com>
2019-02-10 12:59:23 +0530

    Fix email

--- config/mail.php
+++ config/mail.php
@@ -29,7 +29,7 @@ return [
     |
     */
 
-    'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+    'host' => env('MAIL_HOST', 'gator3224.hostgator.com'),
 
     /*
     |--------------------------------------------------------------------------
